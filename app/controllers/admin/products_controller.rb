66d4fcde3735332e9795f6ca69ff0e22df277620
ApamAa <apama.jamshidi@gmail.com>
2020-02-06 02:29:14 +0000

    admin authentication

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,9 +1,10 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV['AUTH_USERNAME'].to_s, password: ENV['AUTH_PASSWORD'].to_s
 
   def index
     @products = Product.order(id: :desc).all
   end
-
+git 
   def new
     @product = Product.new
   end
